32ad49085fa79a1bfa5310da678da75eb4479c59
глебас <bulojchik51@gmail.com>
2026-05-11 21:41:59 +0300

    feat: add agencies tab with grid and buttons

--- src/TravelAgency/TravelAgency/Forms/MainForm.Designer.cs
+++ src/TravelAgency/TravelAgency/Forms/MainForm.Designer.cs
@@ -17,6 +17,15 @@
         private System.Windows.Forms.TabPage toursTab;
         private System.Windows.Forms.TabPage bookingsTab;
 
+        // Агентства
+        private System.Windows.Forms.DataGridView agenciesGrid;
+        private System.Windows.Forms.Button addAgencyButton;
+        private System.Windows.Forms.Button editAgencyButton;
+        private System.Windows.Forms.Button deleteAgencyButton;
+        private System.Windows.Forms.TextBox searchAgencyBox;
+        private System.Windows.Forms.Button searchAgencyButton;
+        private System.Windows.Forms.Panel agencyButtonPanel;
+
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -39,6 +48,14 @@
             this.toursTab = new System.Windows.Forms.TabPage();
             this.bookingsTab = new System.Windows.Forms.TabPage();
 
+            this.agenciesGrid = new System.Windows.Forms.DataGridView();
+            this.addAgencyButton = new System.Windows.Forms.Button();
+            this.editAgencyButton = new System.Windows.Forms.Button();
+            this.deleteAgencyButton = new System.Windows.Forms.Button();
+            this.searchAgencyBox = new System.Windows.Forms.TextBox();
+            this.searchAgencyButton = new System.Windows.Forms.Button();
+            this.agencyButtonPanel = new System.Windows.Forms.Panel();
+
             // MenuStrip
             this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
                 this.fileMenu,
@@ -58,7 +75,6 @@
             this.loadMenuItem.Text = "Загрузить";
             this.exitMenuItem.Text = "Выход";
 
-            // Пункты меню
             this.agenciesMenuItem.Text = "Агентства";
             this.toursMenuItem.Text = "Туры";
             this.bookingsMenuItem.Text = "Бронирования";
@@ -73,6 +89,47 @@
             this.agenciesTab.Text = "Агентства";
             this.toursTab.Text = "Туры";
             this.bookingsTab.Text = "Бронирования";
+
+            // Панель кнопок агентств
+            this.agencyButtonPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.agencyButtonPanel.Height = 40;
+
+            this.addAgencyButton.Text = "Добавить";
+            this.addAgencyButton.Width = 90;
+            this.addAgencyButton.Location = new System.Drawing.Point(5, 8);
+
+            this.editAgencyButton.Text = "Редактировать";
+            this.editAgencyButton.Width = 110;
+            this.editAgencyButton.Location = new System.Drawing.Point(100, 8);
+
+            this.deleteAgencyButton.Text = "Удалить";
+            this.deleteAgencyButton.Width = 90;
+            this.deleteAgencyButton.Location = new System.Drawing.Point(215, 8);
+
+            this.searchAgencyBox.Width = 150;
+            this.searchAgencyBox.Location = new System.Drawing.Point(320, 10);
+
+            this.searchAgencyButton.Text = "Поиск";
+            this.searchAgencyButton.Width = 70;
+            this.searchAgencyButton.Location = new System.Drawing.Point(475, 8);
+
+            this.agencyButtonPanel.Controls.AddRange(new System.Windows.Forms.Control[] {
+                this.addAgencyButton,
+                this.editAgencyButton,
+                this.deleteAgencyButton,
+                this.searchAgencyBox,
+                this.searchAgencyButton
+            });
+
+            // DataGridView агентств
+            this.agenciesGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.agenciesGrid.ReadOnly = true;
+            this.agenciesGrid.AllowUserToAddRows = false;
+            this.agenciesGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.agenciesGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+
+            this.agenciesTab.Controls.Add(this.agenciesGrid);
+            this.agenciesTab.Controls.Add(this.agencyButtonPanel);
 
             // MainForm
             this.Text = "Турагентство";
