c9c37927f6b4a44cfc727a5bcd3b7ba44facb6ce
глебас <bulojchik51@gmail.com>
2026-05-11 21:36:30 +0300

    feat: add MainForm and project structure

---
+++ src/TravelAgency/TravelAgency/Forms/MainForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+using TravelAgency.Services;
+using TravelAgency.Storage;
+
+namespace TravelAgency.Forms
+{
+    public partial class MainForm : Form
+    {
+        private readonly AgencyService _agencyService;
+        private readonly TourService _tourService;
+        private readonly BookingService _bookingService;
+        private readonly JsonStorage _storage;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            _storage = new JsonStorage();
+            _agencyService = new AgencyService();
+            _tourService = new TourService();
+            _bookingService = new BookingService();
+        }
+    }
+}
