2c97c394d82c87913b1a93ed7e1d9b4243eda56f
глебас <bulojchik51@gmail.com>
2026-05-11 20:42:51 +0300

    feat: add core models (Tour, Agency, Country, Booking)

---
+++ src/TravelAgency/TravelAgency/Models/Booking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TravelAgency.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public string ClientName { get; set; }
+        public string ClientPhone { get; set; }
+        public Tour Tour { get; set; }
+        public DateTime BookingDate { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ src/TravelAgency/TravelAgency/Models/Tour.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TravelAgency.Models
+{
+    public class Tour
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int DurationDays { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public Country Country { get; set; }
+        public Agency Agency { get; set; }
+    }
+}
